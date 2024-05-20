b2c272d9cfcaef9f717184c00a1e7533a6cb474c
Mahdi-Golriz <mahdi.golriz.1993@gmail.com>
2024-05-20 19:15:08 +0200

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  return <Steps />;
+}
+
+function Steps() {
+  const [step, setStep] = useState(1);
+  const [click, setClick] = useState(true);
+
+  function PreviousHandle() {
+    step > 1 && setStep((st) => st - 1);
+  }
+
+  function NextHandle() {
+    step < 3 && setStep((st) => st + 1);
+  }
+
+  function handleClick() {
+    setClick((i) => !i);
+    setStep(1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="steps">
+      <span className="close" onClick={handleClick}>
+        &times;
+      </span>
+      {click && (
+        <>
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={PreviousHandle}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={NextHandle}
+            >
+              Next
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
-
-export default App;
